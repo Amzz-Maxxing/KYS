0fba3d18cb05a52ab7ade284c91a40e818678b81
Amzz-Maxxing <adham_22010261@utp.edu.my>
2025-02-16 00:42:24 +0800

    CLC, Home, Register

--- KYS - Prototype 1/obj/Debug/net8.0/KYS - Prototype 1.AssemblyInfo.cs
+++ KYS - Prototype 1/obj/Debug/net8.0/KYS - Prototype 1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KYS - Prototype 1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1382ebe3a58d945dfff7478bbd3d3cf022bda631")]
 [assembly: System.Reflection.AssemblyProductAttribute("KYS - Prototype 1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KYS - Prototype 1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
